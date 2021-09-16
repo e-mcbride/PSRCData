16439dc40f164a20c8888f0c743849b592b79393
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-09-15 17:48:16 -0700

    First commit: project structure files

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' PSRC Household-Level Data
+#'
+#' Household-level dataset from the 2017-2019 Pacific Southwest Regional Council Household Travel Survey Public Release 2.
+#'
+#' @format A data frame with 97 variables
+#' @source \url{https://www.psrc.org/household-travel-survey-program}
+"hhdat"
+
+#' PSRC Person-Level Data
+#'
+#' Person-level dataset from the 2017-2019 Pacific Southwest Regional Council Household Travel Survey Public Release 2.
+#'
+#' @format A data frame with 124 variables
+#' @source \url{https://www.psrc.org/household-travel-survey-program}
+"prdat"
+
+#' PSRC Travel Diary Data
+#'
+#' Travel diary dataset from the 2017-2019 Pacific Southwest Regional Council Household Travel Survey Public Release 2.
+#'
+#' @format A data frame with 122 variables
+#' @source \url{https://www.psrc.org/household-travel-survey-program}
+"trdat"
